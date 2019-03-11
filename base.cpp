5abaca5f2f96bd63d421388fb57bc00177549824
igornerest <igornerest@gmail.com>
2019-03-12 00:39:28 +0100

    codigo base

---
+++ base.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long			ll;
+typedef pair<int, int>		ii;
+typedef vector<ii>			vii;
+typedef vector<int>			vi;
+
+#define	INF	0x3f3f3f3f
+#define	pb	push_back
+#define	mk	make_pair
+#define	fi	first
+#define	se	second
+#define	eb	emplace_back
+
+int main() {
+
+	return 0;
+}
